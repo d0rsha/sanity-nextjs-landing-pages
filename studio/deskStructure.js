2c012105bfed776b68760f7a005635a8890abd9f
Anders Vrethem <anders.vrethem@consid.se>
2022-10-19 16:23:39 +0200

    hierarchical-document-list

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,4 +1,5 @@
 import S from '@sanity/desk-tool/structure-builder'
+import {createDeskHierarchy} from '@sanity/hierarchical-document-list'
 
 // We filter document types defined in structure to prevent
 // them from being listed twice
@@ -12,4 +13,25 @@ export default () =>
       S.documentTypeListItem('page').title('Pages'),
       S.documentTypeListItem('route').title('Routes'),
       ...S.documentTypeListItems().filter(hiddenDocTypes),
+
+      createDeskHierarchy({
+        title: 'Main table of contents',
+
+        // The hierarchy will be stored in this document ID 👇
+        documentId: 'main-table-of-contents',
+
+        // Document types editors should be able to include in the hierarchy
+        referenceTo: ['page', 'route', 'site-config'],
+
+        // ❓ Optional: provide filters and/or parameters for narrowing which documents can be added
+        // referenceOptions: {
+        //   filter: 'status in $acceptedStatuses',
+        //   filterParams: {
+        //     acceptedStatuses: ['published', 'approved']
+        //   }
+        // },
+
+        // ❓ Optional: limit the depth of your hierarachies
+        maxDept: 3
+      })
     ])
